c6d3ebc014511f6a1ddd05b266f926af094ba1f8
Ankit Saklani <97657457+Broshli@users.noreply.github.com>
2023-12-25 20:49:11 +0530

    summary

--- lib/singleton1.dart
+++ lib/singleton1.dart
@@ -1,3 +1,23 @@
+// class Singleton1 {
+//   static Singleton1? _instance; // this is an instance of this class
+
+//   Singleton1._internal(); // this is the privatised constructor for this class if we use this so the use or anyone conn't be able to create an instance of this class directly it mean this class now have only one instance.
+
+//   static Singleton1 getInstance() {
+//     return _instance!;
+//   }
+// }
+
+// Singleton1 singleton = Singleton1.getInstance();
+
 class Singleton1 {
-  Singleton1? _instance;
+  static Singleton1? _instance;
+
+  Singleton1._internal();
+
+  static Singleton1 getInstance() {
+    return _instance!;
+  }
 }
+
+Singleton1 singleton1 = Singleton1.getInstance();
